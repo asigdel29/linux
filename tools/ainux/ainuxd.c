5ee12d113df7e5e1d16290ed55ef0856c1f13356
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2025-07-03 00:33:40 -0400

    AINUX scaffolding: ai_core, modelfs, tools

---
+++ tools/ainux/ainuxd.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("ainuxd starting (gRPC mock)\n");
+    /* TODO: implement gRPC server */
+    return 0;
+}
